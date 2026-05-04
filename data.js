31b382cd750aacc10c26bd9a8573566696048e62
maylilllaction <642722474@qq.com>
2026-05-04 10:39:04 +0000

    action:update data

--- data.js
+++ data.js
@@ -2091,8 +2091,93 @@ const eventData = [
     "team": "TEAM B"
   },
   {
+    "time": "2026/05/13 19:30",
+    "theme": "《心的旅程》",
+    "team": "SNH48-新生队"
+  },
+  {
+    "time": "2026/05/14 19:30",
+    "theme": "《Nice to meet you II》",
+    "team": "TEAM NII"
+  },
+  {
+    "time": "2026/05/14 19:30",
+    "theme": "《偶像研究计划H组-心的旅程·第六场》",
+    "team": "GNZ48"
+  },
+  {
+    "time": "2026/05/15 19:30",
+    "theme": "《拾忆：TEAM G·第二场》",
+    "team": "TEAM G"
+  },
+  {
+    "time": "2026/05/15 19:30",
+    "theme": "《 1&1 Anyone》",
+    "team": "SNH48"
+  },
+  {
+    "time": "2026/05/16 13:30",
+    "theme": "《INTO THE LIGHT》",
+    "team": "TEAM SII"
+  },
+  {
+    "time": "2026/05/16 14:00",
+    "theme": "《我的太阳》",
+    "team": "TEAM K"
+  },
+  {
+    "time": "2026/05/16 14:00",
+    "theme": "《幻镜》",
+    "team": "TEAM CII"
+  },
+  {
+    "time": "2026/05/16 18:30",
+    "theme": "《茉莉雨》",
+    "team": "SNH48-林舒晴"
+  },
+  {
     "time": "2026/05/16 19:00",
-    "theme": "《淇・迹》",
+    "theme": "《手牵手》",
+    "team": "TEAM GII"
+  },
+  {
+    "time": "2026/05/16 19:00",
+    "theme": "《淇·迹》",
     "team": "GNZ48-叶舒淇"
+  },
+  {
+    "time": "2026/05/16 19:00",
+    "theme": "《SPICY》",
+    "team": "CKG48"
+  },
+  {
+    "time": "2026/05/17 13:30",
+    "theme": "《Nice to meet you II》",
+    "team": "TEAM NII"
+  },
+  {
+    "time": "2026/05/17 14:00",
+    "theme": "《拾忆：TEAM NIII·第四场》",
+    "team": "TEAM NIII"
+  },
+  {
+    "time": "2026/05/17 14:00",
+    "theme": "《幻镜》",
+    "team": "TEAM CII"
+  },
+  {
+    "time": "2026/05/17 14:00",
+    "theme": "《Fearless》",
+    "team": "CKG48"
+  },
+  {
+    "time": "2026/05/17 18:30",
+    "theme": "《Fire X》",
+    "team": "TEAM X"
+  },
+  {
+    "time": "2026/05/17 19:00",
+    "theme": "《TEAM Z斗宿之诀[2.0]·第一百三十九场》",
+    "team": "TEAM Z"
   }
 ];
